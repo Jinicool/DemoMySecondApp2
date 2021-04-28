d87da0bc10e8890d313b7dbbb5e2c4ecc6644514
20022543 <20022543@myrp.edu.sg>
2021-04-28 22:09:37 +0800

    I have added.

---
+++ app/src/main/java/sg/edu/rp/c346/id20022543/demomysecondapp/MainActivity2a.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.id20022543.demomysecondapp;
+
+public class MainActivity2a {
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id20022543/demomysecondapp/MainActivityLogin.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20022543.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+    }
+}
